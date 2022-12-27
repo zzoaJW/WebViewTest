cf8c10dab90f980b36a642492024b5ec09288ed9
zzoaJW <i7can7i@gmail.com>
2022-12-27 22:50:18 +0900

    load URL by WebView

--- app/src/main/java/com/z0o0a/webviewtest/MainActivity.kt
+++ app/src/main/java/com/z0o0a/webviewtest/MainActivity.kt
@@ -2,6 +2,7 @@ package com.z0o0a.webviewtest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebViewClient
 import com.z0o0a.webviewtest.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -12,5 +13,9 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+
+        binding.webView.webViewClient = WebViewClient()
+        binding.webView.settings.javaScriptEnabled = true
+        binding.webView.loadUrl("https://www.naver.com/")
     }
 }
