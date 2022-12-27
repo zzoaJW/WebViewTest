969a7b4d91fbd8e463f7b7f42cb9b847f4db3661
zzoaJW <i7can7i@gmail.com>
2022-12-27 23:41:06 +0900

    set that WebView reload by SwifeLayout

--- app/src/main/java/com/z0o0a/webviewtest/MainActivity.kt
+++ app/src/main/java/com/z0o0a/webviewtest/MainActivity.kt
@@ -1,13 +1,16 @@
 package com.z0o0a.webviewtest
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.webkit.WebViewClient
+import androidx.annotation.RequiresApi
 import com.z0o0a.webviewtest.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -17,6 +20,13 @@ class MainActivity : AppCompatActivity() {
         binding.webView.webViewClient = WebViewClient()
         binding.webView.settings.javaScriptEnabled = true
         binding.webView.loadUrl("https://www.naver.com/")
+
+        binding.webViewLayout.setOnRefreshListener {
+            binding.webView.reload()
+
+            binding.webViewLayout.isRefreshing = false
+        }
+
 
         binding.btnBack.setOnClickListener {
             binding.webView.goBack()
@@ -29,5 +39,6 @@ class MainActivity : AppCompatActivity() {
         binding.btnRefresh.setOnClickListener {
             binding.webView.reload()
         }
+
     }
 }
