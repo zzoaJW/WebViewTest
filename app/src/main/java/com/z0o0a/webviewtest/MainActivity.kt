3c851cf2c4726e8037dd09c53739c2343e44f220
zzoaJW <i7can7i@gmail.com>
2022-12-27 20:11:27 +0900

    set layout

--- app/src/main/java/com/z0o0a/webviewtest/MainActivity.kt
+++ app/src/main/java/com/z0o0a/webviewtest/MainActivity.kt
@@ -2,10 +2,15 @@ package com.z0o0a.webviewtest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.z0o0a.webviewtest.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
     }
 }
